7fa43885af5e819ef70043aa60514a6152376fc7
truepartner312 <truepartner312@gmail.com>
2024-01-22 21:58:53 +0900

    auth0 update.

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -1,17 +1,9 @@
 import { handleAuth, handleLogin, handleCallback } from '@auth0/nextjs-auth0/edge';
 
-const redirectUri = `/api/auth/callback`;
+// const redirectUri = `/api/auth/callback`;
 
-export const GET = handleAuth({
-  login: handleLogin({
-    authorizationParams: { redirect_uri: redirectUri }
-  }),
-  callback: handleCallback({ redirectUri }),
-  onError(req: Request, error: Error) {
-    console.error(error);
-  }
-});
+export const GET = handleAuth();
 
 export const runtime = 'edge';
 //https://github.com/vercel/next.js/issues/51642
-export const fetchCache = 'force-no-store';
+// export const fetchCache = 'force-no-store';
